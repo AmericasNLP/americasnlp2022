3745de3f6301bf3a74ce53c0260d6e8a15e3fcea
Abteen <abteen.ebrahimi@colorado.edu>
2022-06-08 11:17:41 -0600

    Update random baseline file names; add ASR baseline outputs

--- scripts/run_random_baseline.sh
+++ scripts/run_random_baseline.sh
@@ -5,7 +5,7 @@ set -eou pipefail
 # for l in Bribri Guarani Kotiria Quechua Waikhana; do
 for l in Bribri Kotiria Quechua Waikhana; do
     echo $l
-    python -u baseline.py \
+    python -u baselines/random_baseline.py \
         --train_meta data/$l/train/meta.tsv \
         --test_meta data/$l/dev/meta.tsv \
         --out_fn preds/${l}-dev-speech.txt \
