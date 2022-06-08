0dfc0e05191716fbcf95cdfd0609ed56dde09939
Adamits <ajwieme@gmail.com>
2022-06-08 12:18:57 -0600

    Update run_baseline script - Add both tasks to loop - Change data dir to match download_data script

--- scripts/run_random_baseline.sh
+++ scripts/run_random_baseline.sh
@@ -3,11 +3,14 @@
 set -eou pipefail
 
 # for l in Bribri Guarani Kotiria Quechua Waikhana; do
-for l in Bribri Kotiria Quechua Waikhana; do
-    echo $l
-    python -u baselines/random_baseline.py \
-        --train_meta data/$l/train/meta.tsv \
-        --test_meta data/$l/dev/meta.tsv \
-        --out_fn preds/${l}-dev-speech.txt \
-        --task speech
+mkdir -p preds
+for task in translate speech; do
+    for l in Bribri Kotiria Quechua Waikhana; do
+        echo $l
+        python -u baselines/random_baseline.py \
+            --train_meta Downloads/$l/train/meta.tsv \
+            --test_meta Downloads/$l/dev/meta.tsv \
+            --out_fn preds/${l}-dev-speech.txt \
+            --task speech
+    done
 done
